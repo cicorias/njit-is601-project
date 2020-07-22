dc2293af66cfb24f0358952d7444caf4ae5c6c27
Shawn Cicoria <github@cicoria.com>
2020-07-22 16:48:24 -0400

    updates

--- surveysite/survey/urls.py
+++ surveysite/survey/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='survey-index'),  # views.SurveyList.as_view())  # 
+    path('foo/', views.SurveyList.as_view())
 ]
--- surveysite/survey/views.py
+++ surveysite/survey/views.py
@@ -1,9 +1,28 @@
+from django.http import (HttpResponse,
+                         HttpRequest,
+                         Http404,
+                         HttpResponseRedirect,
+                         JsonResponse)
+
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+from .models import Survey
 
-from django.http import HttpResponse
+from django.views.generic import ListView
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the survey index.")
+class SurveyList(ListView):
+    paginate_by = 2
+    model = Survey
+
+
+# def index2(request: HttpRequest) -> HttpResponse:
+#     return SurveyList()
+
+def index(request: HttpRequest) -> HttpResponse:
+    surveys = Survey.objects.all()
+    context = {'items': surveys}
+
+    return render(request, 'survey/index.html', context)
+    
