2d23c97393bc926e8eb3c13ff6de6554ec17c8ec
Shawn Cicoria <github@cicoria.com>
2020-07-25 13:58:04 -0400

    first run
    added seed data

--- surveysite/first-run.sh
+++ surveysite/first-run.sh
@@ -7,6 +7,9 @@ docker-compose run web python manage.py migrate
 # DJANGO_SUPERUSER_PASSWORD="Foobar1234" docker-compose run web python manage.py createsuperuser --username root --email root@example.com
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('root', 'root@example.com', 'password')" | docker-compose run web python manage.py shell
 
+docker-compose run -e PYTHONPATH='/code'  -e DJANGO_SETTINGS_MODULE='surveysite.container' web django-admin loaddata data-dump.json
+
+
 #  now navigate to http://localhost:8000/admin to create a survey
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
